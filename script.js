74ad35d8ac7164d78b85cd4e20b5d6befa897afb
31dylanj-pixel <31dylan_j@students.ics.ac.th>
2026-09-18 18:33:47 +0700

    Add clearElementSearch variable for search functionality

--- script.js
+++ script.js
@@ -1531,6 +1531,9 @@ document.addEventListener("keydown", event => {
 const elementSearch =
     document.getElementById("element-search");
 
+const clearElementSearch =
+    document.getElementById("clear-element-search");
+
 function getSearchableElements() {
     return document.querySelectorAll(
         ".element, .series-placeholder"
